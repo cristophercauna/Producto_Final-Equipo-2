a516bdd5d39503a4d519253bd012dc13d538912a
cristophercauna <ccaunam@unjbg.edu.pe>
2025-07-11 22:41:22 -0500

    Actualizar DatosAcademia.cpp: Se añade la funcion de mostrarPostulantes para mostrar los postulantes por canal y ciclo

--- DatosAcademia.cpp
+++ DatosAcademia.cpp
@@ -8,7 +8,7 @@ struct Postulante{
         string canal;
     };
     void agregarPostulante(Postulante[], int&, Postulante);
-
+    void mostrarPostulante(Postulante[],int);
 int main(){
     Postulante postulantes[100];
     int n,op,i;
@@ -17,11 +17,13 @@ int main(){
         system("cls");
         cout<<"\n*********** BIENVENIDO A LA ACADEMIA BASADRE ***********\n";
         cout<<"1. Postulante Nuevo\n";
+        cout<<"2. Mostrar Postulantes\n";
         cout<<"0. Salir\n";
         cout<<"Ingrese el operador: "; cin>>op;
         switch(op){
             case 1:{
                 Postulante nuevo;
+                system("cls");
                 cout<<"\n ********* Ingrese los datos del nuevo postulante *********\n";
                 cout<<"Ingrese el DNI: "; cin>>nuevo.DNI;
                 cin.ignore();
@@ -29,6 +31,12 @@ int main(){
                 cout<<"Ingrese el ciclo que ira el Postulante (cepu o fase): "; getline(cin , nuevo.ciclo);
                 cout<<"Ingrese el canal que el postulante seguira: "; getline(cin , nuevo.canal);
                 agregarPostulante(postulantes , numPostulantes , nuevo);
+                system("pause");
+                break;
+            }
+            case 2:{
+                system("cls");
+                mostrarPostulante(postulantes,numPostulantes);
                 system("pause");
                 break;
             }
@@ -48,5 +56,32 @@ void agregarPostulante(Postulante lista[], int& cantidad, Postulante nuevo){
         cout<<"Los datos del Postualante fueron agregados correctamente!!"<<endl;
     }else{
         cout<<"Ya nos se pueden agregar mas postulantes"<<endl;
+    }
+}
+void mostrarPostulante(Postulante lista[],int cantidad){
+    string ciclo[]={"cepu","fase"} ;
+    string canales[]={"1","2","3","4"};
+    for(int i=0; i<2 ; i++){ //ESTO ES PARA QUE LOS CICLOS AVANCEN
+        for(int j=0 ; j<4 ; j++){ //ESTO ES PARA QUE LOS CANALES AVANCEN
+            cout<<"Ciclo: "<<ciclo[i]<<" | "<<"Canal :"<<canales[j]<<endl;
+            cout<<"--------------------------------------------------"<<endl;
+            int encontrado=0;
+            for(int k=0 ; k < cantidad ; k++){
+                if(lista[k].ciclo == ciclo[i] && lista[k].canal == canales[j]){
+                    cout<<"DNI: "<<lista[k].DNI<<"  ";
+                    cout<<"Nombres: "<<lista[k].nomCompletos<<"  "<<endl;
+                    cout<<"----------------------------------------"<<endl;
+                    encontrado=1;
+                }
+            }
+            if(encontrado==0){
+                cout<<"Sin postulantes en este grupo"<<endl;
+                cout<<"----------------------------------------"<<endl;
+            }
+        }
+        
+    }
+    if(cantidad == 0){
+        cout<<"No hay postulantes"<<endl;
     }
 }
